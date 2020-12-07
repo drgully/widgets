e25d4748f072a1ca137623256cdfe446cb6c76ed
drgully <david.r.gully@gmail.com>
2020-12-07 17:02:29 -0300

    completion of the dropdown widget and addition of the translate widget

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Accordion from './components/Accordion';
-import Search from './components/Search';
+// import Search from './components/Search';
+// import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 /* const items = [
   {
@@ -17,11 +19,42 @@ import Search from './components/Search';
   }
 ]; */
 
-export default () => {
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'The Color Blue',
+    value: 'blue'
+  }
+];
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
-      {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Accordion items={items} />
+      <Search /> 
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}*/}
+      <Translate />
     </div>
   );
 };
+
+export default App;
